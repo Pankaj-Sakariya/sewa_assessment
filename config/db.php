de1679863c065cd0017a41262071dd93710cdf46
Pankaj-Sakariya <pankaj10it45@gmail.com>
2016-04-06 14:09:54 +0530

    Update db.php
    DB chnage from server

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sewa_assessment',
+    'dsn' => 'mysql:host=192.168.1.144;dbname=sewa_assessment',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
